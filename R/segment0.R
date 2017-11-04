6e1748b5654874b1858891faa786258121b06a5f
caimiao0714 <miaocai@slu.edu>
2017-11-04 16:16:19 -0500

    initialisation of the MiaoTruck project

---
+++ R/segment0.R
@@ -0,0 +1,23 @@
+#' FUNCTION "segment_0"
+#' Define a funtion "segment0" that separate different shifts
+Imports:
+  data.table
+#' @export
+segment_0 = function(speed, threshold, time_diff) {
+  ## Replace very long single points
+  speed[time_diff >= threshold] <- 0
+  ## First, replacing stretches of less than "threshold" consecutive 0 speeds by 1s
+  r1 = rle(speed != 0)
+  r1$values <- replicate(length(r1$values), 1)
+  r1$values <- cumsum(r1$values)
+  order_tmp <- inverse.rle(r1)
+  dat_tmp1 <- data.table::data.table(speed, order_tmp, time_diff)
+  dat_tmp2 <- dat_tmp1[,.(sumdiff = sum(time_diff)), by = order_tmp]
+  r2 = rle(speed != 0)
+  r2$values[r2$values == 0 & dat_tmp2$sumdiff < threshold] <- TRUE
+  r2 <- inverse.rle(r2)
+  r2 <- rle(r2)
+  ## Then numbering consecutive stretches of non-zero values
+  r2$values[r2$values] = cumsum(r2$values[r2$values])
+  return(inverse.rle(r2))
+}
---
+++ R/tidy_data.R
@@ -0,0 +1,49 @@
+# Function "tidy_data"
+
+
+tidy_data <- function(data, delete_duplicate = TRUE){
+  require(dplyr)
+  require(lubridate)
+  data <- data[!is.na(data$DATIME_GMT),]
+  data$DATEGMT_lub <- suppressWarnings(lubridate::mdy_hm(data$DATIME_GMT))
+  data <- data[!is.na(data$DATEGMT_lub),]
+  dat_nodup <- data
+  ####data$c_datime <- lubridate::mdy_hm(data$c_datime)
+  # order time points
+
+  # deleting duplicate time points
+  if(delete_duplicate){
+    dat_nodup <- data[!duplicated(data$c_datime)|is.na(data$c_datime),]
+    dat_nodup <- dat_nodup[!duplicated(dat_nodup$DATEGMT_lub),]
+  }else{
+    dat_nodup <- dat_nodup
+  }
+  dat_nodup <- arrange(dat_nodup, DATEGMT_lub)
+
+  # filtering only the "hard breaks"
+  # data <-
+  #   data[!(data$EVT_TYP == "ROLL_STABILITY" |
+  #   data$EVT_TYP == "HEADWAY") | is.na(data$EVT_TYP), ]
+  dat_nodup <-
+    dat_nodup[(dat_nodup$EVT_TYP != "ROLL_STABILITY" &
+                 dat_nodup$EVT_TYP != "HEADWAY") | is.na(dat_nodup$EVT_TYP), ]
+
+  #create indicating variable of "hard brakes"
+  # data$hard_brake <- 0
+  # data$hard_brake[data$EVT_TYP == "HARD_BRAKING"] <- 1
+  dat_nodup$hard_brake <- 0
+  # dat_nodup$hard_brake[dat_nodup$EVT_TYP == "HARD_BRAKING"] <- 1
+  dat_nodup$hard_brake[!is.na(dat_nodup$EVT_TYP)] <- 1
+
+  # create a new variable of time difference
+  # data <- arrange(data, DATEGMT_lub)
+  # data$time_diff <- data$DATEGMT_lub - lag(data$DATEGMT_lub)
+  # data$time_diff[1] <- 0
+  # data$time_diff <- as.numeric(data$time_diff)/60
+  dat_nodup <- arrange(dat_nodup, DATEGMT_lub)
+  dat_nodup$time_diff <- dat_nodup$DATEGMT_lub - lag(dat_nodup$DATEGMT_lub)
+  dat_nodup$time_diff[1] <- 0
+  dat_nodup$time_diff <- as.numeric(dat_nodup$time_diff)/60
+
+  return(dat_nodup)
+}
